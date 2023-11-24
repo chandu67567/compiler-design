7e3b5fc3236dd12f80d5ef3b54ea0f2568d98862
chandu67567 <132892059+chandu67567@users.noreply.github.com>
2023-11-24 10:14:16 +0530

    Create 32.sum.c

---
+++ 32.sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int sum=0,a,b;
+	printf("enter a number a\t\n");
+	scanf("%d",&a);
+		printf("enter a number b\t\n");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("sum = %d",sum);
+	return 0;
+}
